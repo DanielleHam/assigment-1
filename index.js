06362390d5fab1a17641a773e4b37799f51009fa
Danielle Nilsson Hamrin <ninoue.hamrin@gmail.com>
2023-01-11 14:24:21 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+window.onload = function () {
+  getComputers();
+};
+
+async function getComputers() {
+  let computers = await fetch(
+    "https://hickory-quilled-actress.glitch.me/computers"
+  ).then((response) => response.json());
+
+  let options = "";
+
+  for (let computer of computers) {
+    let title = computer.title;
+    let id = computer.id;
+
+    options += `<option value="${id}">${title}</option>`;
+  }
+
+  document.getElementById("computerName").innerHTML = options;
+
+  console.log(computers);
+  return computers;
+}
